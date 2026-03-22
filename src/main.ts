5c32ef333ac64337410808f2dadf46c1aa86321c
webLXM <1944754630@qq.com>
2026-03-22 14:48:06 +0800

    基础设施：接入Swagger文档

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/filters/all-exceptions.filter';
 
@@ -17,9 +18,23 @@ async function bootstrap() {
   }));
   app.useGlobalFilters(new AllExceptionsFilter());
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('LuckyColor Admin Serve API')
+    .setDescription('LuckyColor 多租户后台服务接口文档')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument, {
+    swaggerOptions: {
+      persistAuthorization: true
+    }
+  });
+
   const port = Number(process.env.PORT || 3001);
   await app.listen(port);
   Logger.log(`Server running at http://127.0.0.1:${port}/api`, 'Bootstrap');
+  Logger.log(`Swagger docs at http://127.0.0.1:${port}/docs`, 'Bootstrap');
 }
 
 void bootstrap();
